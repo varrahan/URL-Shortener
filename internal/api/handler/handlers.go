1e5a37ecda7181bfcccb898b9267ccdc23f02c3b
Varrahan <varrahan@gmail.com>
2025-11-17 21:50:08 -0500

    Add handler

---
+++ internal/api/handler/handlers.go
@@ -0,0 +1,37 @@
+package handler
+
+import (
+	"github.com/varrahan/url-shortener/internal/api/shortener"
+	"github.com/varrahan/url-shortener/internal/api/store"
+	"github.com/varrahan/url-shortener/internal/api/utils"
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+type UrlCreationRequest struct {
+	LongUrl string `json:"long_url" binding:"required"`
+	UserId string `json:"user_id" binding:"required"`
+}
+
+func CreateShortUrl(c *gin.Context) {
+	var creationRequest UrlCreationRequest
+	if err := c.ShouldBindJSON(&creationRequest); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	shortUrl := shortener.GenerateShortLink(creationRequest.LongUrl, creationRequest.UserId)
+	store.SaveUrlMapping(shortUrl, creationRequest.LongUrl, creationRequest.UserId)
+	host := utils.GetEnv("INTERNAL_ADDR", "0.0.0.0:9000")
+
+	c.JSON(200, gin.H{
+		"message":   "short url created successfully",
+		"short_url": host +shortUrl,
+	})
+}
+
+func HandleShortUrlRedirect(c *gin.Context) {
+	shortUrl := c.Param("shortUrl")
+	initialUrl := store.RetrieveInitialUrl(shortUrl)
+	c.Redirect(302, initialUrl)
+}
